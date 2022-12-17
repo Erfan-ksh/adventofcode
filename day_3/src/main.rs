fd7e097f119eb239d4ebc3ca576ed849f58e9e21
erfan <erfanmirzazadeh9@gmail,com>
2022-12-17 23:09:21 +0330

    solved day 3 p1 adn p2

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -3,15 +3,26 @@ fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
     let mut all_shared_chs: Vec<char> = Vec::new();
     let alphabet: Vec<char> = ('a'..='z').chain('A'..='Z').collect();
+
+    let mut line_counter: Vec<&str> = Vec::new();
     for line in input.lines() {
-        let (first, second) = line.split_at(line.len() / 2);
-        for ch in first.chars() {
-            if second.contains(ch) {
-                all_shared_chs.push(ch);
-                break;
+        if line_counter.len() == 2 {
+            line_counter.push(line);
+            println!("{:?}", line_counter);
+            'inner: for ch in line_counter[0].chars() {
+                if line_counter[1].contains(ch) {
+                    if line_counter[2].contains(ch) {
+                        all_shared_chs.push(ch);
+                        line_counter = Vec::new();
+                        break 'inner;
+                    }
+                }
             }
+        } else {
+            line_counter.push(line)
         }
     }
+
     let mut char_to_point: Vec<usize> = Vec::new();
     for ch in all_shared_chs.iter() {
         char_to_point.push(alphabet.iter().position(|x| x == ch).unwrap() + 1);
